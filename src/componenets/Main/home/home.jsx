b2091e3ad8e24ce00fc534c923a9488cba012d5f
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-10-05 13:05:42 +0530

    Completed the Profile section via Prism

--- src/componenets/Main/home/home.jsx
+++ src/componenets/Main/home/home.jsx
@@ -1,35 +1,31 @@
 
 import Prism from "prismjs";
 import "../../../assets/styles/prism-holi-theme.css"
-import "../../../assets/styles/prism-holi-theme-light.css"
 
 import "prismjs/components/prism-javascript"; // lang.
 import { useEffect } from "react";
 
 
-const code = {
-    name: 'Nazmul Hossain',
-    title: 'Full-Stack Developer | Cloud Enthusiast | Problem Solver',
-    skills: [
-        'React', 'NextJS', 'Redux', 'Express',
-        'MySQL', 'MongoDB', 'Docker', 'AWS', 'TypeScript',
-        'GraphQL', 'Git', 'Linux', 'Discord Development'
-    ],
-    hardWorker: true,
-    quickLearner: true,
-    problemSolver: true,
-    yearsOfExperience: 4,
-    hireable: function () {
-        return (
-            this.hardWorker &&
-            this.problemSolver &&
-            this.skills.length >= 5 &&
-            this.yearsOfExperience >= 3
-        );
-    }
-};
+const codeString = `const Profile = {
+  name: 'Kunal Rathore',
+  title: 'Full-Stack Developer | Problem Solver',
+  skills: [
+    'React', 'TypeScript', 'Express', 'Node.js',
+    'MySQL', 'MongoDB', 'Redis',
+    'AWS', 'GraphQL', 'Figma',
+    'Git', 'Linux'....
+  ],
+  problemSolver: true,
+  hireable: function () {
+    return (
+      this.hardWorker &&
+      this.problemSolver &&
+      this.skills.length >= 5 &&
+    );
+  }
+};`;
 
-const codeString = `const code = ${JSON.stringify(code, null, 2)}`
+
 
 export const HomePage = () => {
 
@@ -40,10 +36,10 @@ export const HomePage = () => {
     return (
         <div className="h-screen flex flex-col justify-center mx-9">
 
-            <section className="flex flex-row items-start gap-8 justify-between " >
-                <div className="flex flex-col justify-start mt-30  h-full gap-4 ">
+            <section className="flex flex-row items-start justify-between " >
+                <div className="flex flex-col justify-start my-18 h-full gap-4 ">
                     <div>
-                        <h1 className="text-7xl font-[400]">Hello,</h1>
+                        <h1 className="text-8xl font-[400]">Hello,</h1>
                         <h1 className="text-9xl font-[500]"> I'm Kunal</h1>
                     </div>
                     <pre >I work with React Ecosystem, and write to teach people
@@ -54,15 +50,16 @@ export const HomePage = () => {
                     </pre>
                 </div>
 
-                <div className='w-[650px] mr-4'>
-                    <pre >
+
+                <div className="bg-gradient-to-r from-purple-400 via-violet-400 to-pink-400 p-1 rounded-xl mr-12">
+                    <pre className="!m-0">
                         <code className="language-javascript">
                             {codeString}
                         </code>
                     </pre>
                 </div>
-            </section>
+            </section >
 
-        </div>
+        </div >
     )
 }
