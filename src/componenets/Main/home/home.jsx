a70092511efc0fe7fc72235ce8f5011366b19708
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-10-20 14:35:06 +0530

    image added in about me section, next work on implementing routes working

--- src/componenets/Main/home/home.jsx
+++ src/componenets/Main/home/home.jsx
@@ -5,6 +5,7 @@ import "../../../assets/styles/prism-holi-theme.css"
 
 import "prismjs/components/prism-javascript"; // importing language
 import { useEffect } from "react";
+import profileImg from "../../../assets/profile.png"
 
 import { Linkedin, Github, Mail } from "lucide-react";
 
@@ -61,23 +62,14 @@ export const HomePage = () => {
                     </pre>
                 </div>
             </section >
-            {/* first section which will shown on landing */}
-            <section className="flex flex-row  items-center justify-end py-30" >
+            {/* second section  */}
+            <section className="flex flex-row  items-center justify-evenly py-30" >
 
-                {/* left part for greeting and name */}
+                {/* image div */}
+                <img src={profileImg} alt="profileImg" className="rounded-xl h-100  shadow-xl" />
+                {/* rigth part for about me */}
                 <div className="flex flex-col justify-center  gap-4   w-[50%] ">
-                    <div className="flex flex-col w-150 font-light dark:font-extralight">
-                        <span className="text-4xl ">About Me- </span>
-                        <span className="text-xl">
-                            <p>
-                                <br />
-                                I'm a pre-final year B.Tech student specializing in Information Technology, passionate about solving real-world problems through technology. With a strong foundation in programming, data structures, and web development, I'm continuously exploring new technologies and working on projects that challenge me to grow.
-                            </p>
-                            <br />
-                            <p> Currently honing my skills in <span className=" text-fuchsia-600  dark:text-lime-500 dark:font-light"> Web Development </span>, I'm actively looking for internship opportunities to apply my knowledge in real-world scenarios and contribute to impactful projects. I thrive in collaborative environments, enjoy learning from others, and am eager to take on challenges that push my boundaries.</p>
-
-                        </span>
-                    </div>
+                    <AboutComp />
                 </div>
             </section >
         </div>
@@ -129,4 +121,19 @@ const ContactDiv = (props) => {
             </span>
         </a>
     )
+}
+
+const AboutComp = () => {
+    return <div className="flex flex-col w-150 font-light dark:font-extralight">
+        <span className="text-4xl ">About Me- </span>
+        <span className="text-xl">
+            <p>
+                <br />
+                I'm a pre-final year B.Tech student specializing in Information Technology, passionate about solving real-world problems through technology. With a strong foundation in programming, data structures, and web development, I'm continuously exploring new technologies and working on projects that challenge me to grow.
+            </p>
+            <br />
+            <p> Currently honing my skills in <span className=" text-fuchsia-600  dark:text-lime-500 dark:font-light"> Web Development </span>, I'm actively looking for internship opportunities to apply my knowledge in real-world scenarios and contribute to impactful projects. I thrive in collaborative environments, enjoy learning from others, and am eager to take on challenges that push my boundaries.</p>
+
+        </span>
+    </div>
 }
