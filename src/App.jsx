25af70d343cfc3f5a6436e212bb6367b5a0741a9
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-11-11 20:11:32 +0530

    oneko cat added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,13 @@ import { Nav } from "./components/Header-Nav/nav.jsx";
 //icons 
 import { SunMedium, Moon } from "lucide-react";
 
+
+import { ReactLenis, useLenis } from "lenis/react";
+
+function ScrollHandler() {
+    useLenis((lenis) => { })
+}
+
 import { ErrorPage } from "./components/Main-Pages/ErrorPage.jsx";
 import { MainComp } from "./components/Main-Pages/Main.jsx";
 import { ScrollContextProvider } from "./context/ScrollContext.jsx";
@@ -26,36 +33,38 @@ export default function App() {
 
     return (
         <BrowserRouter>
+            <ReactLenis root options={{ smoothWheel: true, duration: 3.7 }} >
+                <ScrollHandler />
+                <div className="w-screen flex box-border ">
 
-            <div className="w-screen flex box-border ">
+                    <div className="m-5 text-lgflex justify-center fixed gap-2 bg-gray-800  dark:bg-black text-emerald-300 dark:text-lime-400  p-2 ring-2 rounded-xl z-111" > Project status- Working... </div>
 
-                <div className="m-5 text-lgflex justify-center fixed gap-2 bg-gray-800  dark:bg-black text-emerald-300 dark:text-lime-400  p-2 ring-2 rounded-xl z-111" > Project status- Working... </div>
+                    {/* dark mode toggle button */}
+                    <button className="fixed z-12 dark:bg-black border-2 dark:border-white right-10 top-5 bg-gray-50 p-2 shadow-md rounded-lg"
+                        onClick={toggleMode}>
+                        {isDarkMode ? <SunMedium strokeWidth={1.5} /> : <Moon strokeWidth={1.5} />}
+                    </button>
 
-                {/* dark mode toggle button */}
-                <button className="fixed z-12 dark:bg-black border-2 dark:border-white right-10 top-5 bg-gray-50 p-2 shadow-md rounded-lg"
-                    onClick={toggleMode}>
-                    {isDarkMode ? <SunMedium strokeWidth={1.5} /> : <Moon strokeWidth={1.5} />}
-                </button>
+                    <ScrollContextProvider>
+                        {/* navbar */}
+                        <NavToggleContextProvider>
+                            <Nav></Nav>
 
-                <ScrollContextProvider>
-                    {/* navbar */}
-                    <NavToggleContextProvider>
-                        <Nav></Nav>
+                            {/* Main pages*/}
+                            <Routes>
+                                <Route path="/" element={
+                                    <div className="w-full flex flex-col text-black bg-white dark:text-white dark:bg-black">
+                                        <MainComp />
+                                        <Footer />
+                                    </div>
+                                } />
+                                <Route path="/*" element={<ErrorPage />} />
+                            </Routes>
+                        </NavToggleContextProvider>
+                    </ScrollContextProvider>
 
-                        {/* Main pages*/}
-                        <Routes>
-                            <Route path="/" element={
-                                <div className="w-full flex flex-col text-black bg-white dark:text-white dark:bg-black">
-                                    <MainComp />
-                                    <Footer />
-                                </div>
-                            } />
-                            <Route path="/*" element={<ErrorPage />} />
-                        </Routes>
-                    </NavToggleContextProvider>
-                </ScrollContextProvider>
-
-            </div >
+                </div >
+            </ReactLenis >
         </BrowserRouter >
 
     )
--- src/components/Main-Pages/Main.jsx
+++ src/components/Main-Pages/Main.jsx
@@ -3,24 +3,18 @@ import { AboutPage } from "./About.jsx";
 import { ProjectsPage } from "./Projects.jsx";
 import { useScrollContext } from "@/context/ScrollContext.jsx";
 
-import { ReactLenis, useLenis } from "lenis/react";
 import { useNavToggleContextProvider } from "@/context/NavToggleContext.jsx";
 
 export const MainComp = () => {
     const { homeRef, AboutRef, ProjectsRef, ReadsRef } = useScrollContext();
 
     const { toggle } = useNavToggleContextProvider();
-    function ScrollHandler() {
-        useLenis((lenis) => { })
-    }
 
 
-    return <ReactLenis root options={{ smoothWheel: true, duration: 4.5 }} >
-        <ScrollHandler />
-        <main className={`w-full flex flex-col pl-10 pr-18  transition-all duration-1000 ${toggle ? "pl-[10vw]" : "pl-[12vw]"}`}>
-            <div className="py-30" ref={homeRef} ><HomePage /></div>
-            <div className="py-30" ref={AboutRef} ><AboutPage /></div>
-            <div className="py-20" ref={ProjectsRef} ><ProjectsPage /></div>
-        </main>
-    </ReactLenis>
+
+    return <main className={`w-full flex flex-col pl-10 pr-18  transition-all duration-1000 ${toggle ? "pl-[10vw]" : "pl-[12vw]"}`}>
+        <div className="py-30" ref={homeRef} ><HomePage /></div>
+        <div className="py-30" ref={AboutRef} ><AboutPage /></div>
+        <div className="py-20" ref={ProjectsRef} ><ProjectsPage /></div>
+    </main>
 }
